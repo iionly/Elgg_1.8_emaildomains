3133ae9d14a62b35ce0c7aa8329609a97c8fe168
iionly <iionly@gmx.de>
2013-06-18 23:57:23 +0200

    Initial release for Elgg 1.8

---
+++ emaildomains/languages/en.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Elgg email domains language pack.
+ * 
+ * @package ElggEmailDomains
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Curverider Ltd
+ * @copyright Curverider Ltd 2008-2013
+ * @link http://elgg.com/
+ *
+ * Updated for Elgg 1.8 by iionly
+ * iionly@gmx.de
+ */
+
+$english = array(
+'admin:users:emaildomains' => 'Emaildomains',
+'emaildomains:description' => 'If you like, you can only let people with certain email addresses register for your site. For example, you could restrict the site to users with email addresses from yourcompany.com. To do this, enter the email domain (the part after the \'@\') below. To use more than one, separate them with commas.',
+
+'emaildomains:allow' => 'Allow only',
+'emaildomains:deny' => 'Deny from',
+
+'emaildomains:save:failed' => 'Could not save your email domains.',
+'emaildomains:save:success' => 'Email domains saved.',
+);
+
+add_translation("en",$english);
